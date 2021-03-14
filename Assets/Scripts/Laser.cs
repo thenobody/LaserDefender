3c46d35280ecf92b9707dc34504ec3ee2687c924
Anton Vanco <avanco@stackstate.com>
2021-03-14 12:27:27 +0100

    Adding player motion controls

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+  // state
+  private Vector2 velocity;
+  private float yMax;
+
+  // references
+  private Camera gameCamera;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    gameCamera = Camera.main;
+    SetUpMoveBoundaries();
+  }
+  private void SetUpMoveBoundaries()
+  {
+    var maxPoints = gameCamera.ViewportToWorldPoint(new Vector2(1, 1));
+    var spriteSize = GetComponent<SpriteRenderer>().size;
+    var yOffset = spriteSize.y / 2;
+    yMax = maxPoints.y + yOffset;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (IsOutside())
+    {
+      Destroy(gameObject);
+    }
+    else
+    {
+      Move();
+    }
+
+  }
+
+  private bool IsOutside()
+  {
+    return transform.position.y >= yMax;
+  }
+
+  private void Move()
+  {
+    var xDelta = velocity.x * Time.deltaTime;
+    var yDelta = velocity.y * Time.deltaTime;
+    var xNext = transform.position.x + xDelta;
+    var yNext = transform.position.y + yDelta;
+
+    transform.position = new Vector2(xNext, yNext);
+  }
+
+  public void SetSpeed(float laserSpeed)
+  {
+    var defaultX = 0;
+    var defaultY = laserSpeed;
+
+    var angleRadians = transform.rotation.eulerAngles.z * Mathf.Deg2Rad;
+    var sin = Mathf.Sin(angleRadians);
+    var cos = Mathf.Cos(angleRadians);
+
+    var x = cos * defaultX - sin * defaultY;
+    var y = sin * defaultX + cos * defaultY;
+
+    velocity = new Vector2(x, y);
+  }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,67 @@
-﻿using System.Collections;
+﻿// using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+  // config params
+  [SerializeField] float moveSpeed;
+  [SerializeField] float laserSpeed;
+  [Range(0f, 360f)] [SerializeField] float laserAngle;
+  [Range(0f, 1f)] [SerializeField] float offset;
+  [SerializeField] Laser laserPrefab;
+  [SerializeField] int autoShootFrequency;
+
+  // state
+  private float xMin;
+  private float yMin;
+  private float xMax;
+  private float yMax;
+
   // Start is called before the first frame update
   void Start()
   {
+    SetUpMoveBoundaries();
+  }
 
+  private void SetUpMoveBoundaries()
+  {
+    var minPoints = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+    var maxPoints = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+    var spriteSize = GetComponent<SpriteRenderer>().size;
+    var xOffset = spriteSize.x / 2 + offset;
+    var yOffset = spriteSize.y / 2 + offset;
+    xMin = minPoints.x + xOffset;
+    yMin = minPoints.y + yOffset;
+    xMax = maxPoints.x - xOffset;
+    yMax = maxPoints.y - yOffset;
   }
 
   // Update is called once per frame
   void Update()
   {
-
+    Move();
+    if (Random.Range(0, autoShootFrequency) == 0)
+    {
+      FireLaser();
+    }
   }
+  private void FireLaser()
+  {
+    var laser = Instantiate(laserPrefab, transform.position, Quaternion.Euler(0, 0, laserAngle));
+    laser.SetSpeed(laserSpeed);
+  }
+
+  private void Move()
+  {
+    var deltaFactor = Time.deltaTime * moveSpeed;
+    var deltaX = Input.GetAxis("Horizontal") * deltaFactor;
+    var deltaY = Input.GetAxis("Vertical") * deltaFactor;
+    var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+    var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+    transform.position = new Vector2(newXPos, newYPos);
+  }
+
+
 }
